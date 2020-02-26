93db832587be158185058701d443d82322466bd1
lichunlong <lichunlong@mafengwo.com>
2020-02-26 17:36:46 +0800

    init

---
+++ src/main/java/com/kk/date/DateUtil.java
@@ -0,0 +1,19 @@
+package com.kk.date;
+
+import java.time.LocalDate;
+
+/**
+ * @Author: kk
+ * @Date: 2020-02-26 17:15
+ * @Doc_URL https://mp.weixin.qq.com/s/QGm2HxQGYbEdbW7SATMU7Q
+ */
+public final class DateUtil {
+
+    /**
+     * @return 2020-02-02
+     */
+    public static LocalDate getToday() {
+        return LocalDate.now();
+    }
+
+}
